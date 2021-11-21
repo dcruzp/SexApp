9b5f7cb12998036d420a16a6b944123190741f4d
Daniel <danieldelacruzprieto@gmail.com>
2021-11-20 23:25:45 -0500

    annadiendo una pagina para mostrar las figuras ademas de cambiar y poner una descricion de las figuras en la base de datos

--- db/dbmanager.py
+++ db/dbmanager.py
@@ -1,29 +1,35 @@
 import sqlite3
 from pprint import pprint
-from Scrapping.scrapping import get_posturas
 
-conn = sqlite3.connect('sexapp.db')
-c = conn.cursor() 
+dblocation = "C:\\Users\\Daniel\\Documents\\University\\Matematica Aplicada\\SexApp\\db\\sexapp.db"
 
-def create_table (): 
+def create_table (conn):
+  c = conn.cursor()
   c.execute("CREATE TABLE IF NOT EXISTS posturas (id INTEGER, name TEXT , source TEXT, description TEXT)")
 
-def data_entry(tabla , data):
-  for item in data:
-    print(f'INSERT INTO {tabla} VALUES({item[0]},"{item[1]}","{item[2]}", "{item[3]}")')   
-    c.execute(f'INSERT INTO {tabla} VALUES({item[0]},"{item[1]}","{item[2]}", "{item[3]}")')
-
-  conn.commit()
+# def data_entry(tabla , data):
+#   for item in data:
+#     print(f'INSERT INTO {tabla} VALUES({item[0]},"{item[1]}","{item[2]}", "{item[3]}")')   
+#     c.execute(f'INSERT INTO {tabla} VALUES({item[0]},"{item[1]}","{item[2]}", "{item[3]}")')
+  # conn.commit()
   
-def drop_table (name):
+def drop_table (name, conn):
+  c = conn.cursor()
   c.execute(f'DROP TABLE "{name}"') 
 
+def getpostures():
+  conn = sqlite3.connect(dblocation)
+  c = conn.cursor() 
+  c.execute("SELECT name,source FROM posturas")
+  rows = c.fetchall()
+  postures = list(map(lambda x: x[1] , rows))
+  c.close()
+  conn.close()
+  return postures
 
-# posturas = get_posturas()
-# create_table()
-# data_entry('posturas', posturas)
 
-c.close()
-conn.close()
+if __name__ == '__main__':
+  
+  posturas = getpostures()
 
-
+  pprint (posturas)
--- sexapp.py
+++ sexapp.py
@@ -1,9 +1,12 @@
 from datetime import time
+from os import popen
+from pprint import pprint
+from urllib.parse import quote
 from scipy.optimize import linprog 
 import numpy as np
 import pandas as pd
 import streamlit as st
-
+import sqlite3
 
 persona = ["Pedro" , "Alicia"]
 posturas = ["P1", "P2", "P3"]
@@ -21,91 +24,122 @@ bound = (0,None)
 
 
 
-
-energia = pd.DataFrame(
-  Cij,
-  columns= posturas,
-)
-
-
-placer = pd.DataFrame(
-  Paij,
-  columns= posturas
-)
+energia = pd.DataFrame(Cij,columns= posturas,)
+placer = pd.DataFrame(Paij, columns= posturas)
+energiaInicial = pd.DataFrame (E0j, index= persona)
+placerInicial = pd.DataFrame(Pp0j, index= persona)
 
 
-energiaInicial = pd.DataFrame (
-  E0j, 
-  index= persona
-)
-
-placerInicial = pd.DataFrame(
-  Pp0j,
-  index= persona
-)
-
+st.set_page_config(page_title='Sex App')
 st.title('Sex App')
 st.sidebar.header("Sex App")
 
+dblocation = "db\\sexapp.db"
 
-optionsPositions = st.multiselect(
-  'Selecciona las posturas', 
-  ['La bicicleta' , 'Cara a Cara' , 'El Enchufe' , 'Perro tumbado' , 'Pequenna Cuchara' , 'EL puente 69' ]
-)
+choice = st.sidebar.selectbox('Select view' ,['Main' , 'Postures'])
 
-st.subheader('Energia consumida por unidad de tiempo')
-st.dataframe(energia)
+def get_postures_info():
+  conn = sqlite3.connect(dblocation)
+  cursor = conn.cursor() 
+  query = cursor.execute('SELECT name,source,description FROM posturas')
+  rows = query.fetchall()
+  names = list(map(lambda x : x[0], rows))
+  sources = list(map(lambda x : x[1], rows))
+  descriptions = list(map(lambda x : x[2], rows))
+  return (names, sources ,descriptions)
 
+def show_postures():
+  names, sources , descriptions =  get_postures_info()
+  
+  for i in range (len(names)):
+    with st.expander(names[i]):
+      st.write(descriptions[i])
+      st.image(sources[i])
 
-st.subheader ('Placer generado por unidad de tiempo')
-st.dataframe(placer)
+# esto es de prueba 
+#obteniendo posturas de la base de datos ----------------------------------------------
 
+def show_main_page(): 
 
-st.subheader('Energia inicial de los participantes')
-st.bar_chart(energiaInicial,use_container_width=False)
+  conn = sqlite3.connect(dblocation)
+  cursor = conn.cursor()
 
-st.subheader('Placer inicail de los particiapantes')
-st.bar_chart(placerInicial,use_container_width=False)
+  query = cursor.execute('SELECT name FROM posturas')
+  rows = query.fetchall()
+  postures = list(map(lambda x: x[0] , rows))
+  cursor.close()
+  conn.close()
 
-
-
-result= linprog(c = c , A_ub= A_ub, b_ub = b_ub , bounds= bound, method='simplex')
-st.subheader('Tiempo dedicado a cada postura')
-
-timeresult = pd.DataFrame(
-  result.x,
-  index= posturas,
-)
-
-st.line_chart(timeresult)
-
-
-
-title = st.text_input('Movie title ' , 'Life of Brian')
-st.write('The current movie title is', title) 
-
-values = st.slider(
-  'Select a range of values', 
-  0.0,100.0, (25.0,75.0)
-)
+  optionsPositions = st.multiselect('Selecciona las posturas', postures , help="Esto es una ayuda")
 
 
-col1, col2 , col3 = st.columns(3)
 
-with col1:
-    with st.form('Form1'):
-        st.selectbox('Select flavor', ['Vanilla', 'Chocolate'], key=1)
-        st.slider(label='Select intensity', min_value=0, max_value=100, key=4)
-        submitted1 = st.form_submit_button('Submit 1')
+  st.subheader('Energia consumida por unidad de tiempo')
+  st.dataframe(energia)
 
-with col2:
-    with st.form('Form2'):
-        st.selectbox('Select Topping', ['Almonds', 'Sprinkles'], key=2)
-        st.slider(label='Select Intensity', min_value=0, max_value=100, key=3)
-        submitted2 = st.form_submit_button('Submit 2')
 
-with col3:
-    with st.form('Form3'):
-        st.selectbox('Select Position', ['Perrito', 'Sesentaynueve'], key=2)
-        st.slider(label='Select Intensity', min_value=0, max_value=100, key=3)
-        submitted2 = st.form_submit_button('Submit 2')
+  st.subheader ('Placer generado por unidad de tiempo')
+  st.dataframe(placer)
+
+
+  st.subheader('Energia inicial de los participantes')
+  st.bar_chart(energiaInicial,use_container_width=False)
+
+  st.subheader('Placer inicail de los particiapantes')
+  st.bar_chart(placerInicial,use_container_width=False)
+
+
+
+  result= linprog(c = c , A_ub= A_ub, b_ub = b_ub , bounds= bound, method='simplex')
+  st.subheader('Tiempo dedicado a cada postura')
+
+  timeresult = pd.DataFrame(result.x,index= posturas)
+
+  st.line_chart(timeresult)
+
+
+
+  title = st.text_input('Movie title ' , 'Life of Brian')
+  st.write('The current movie title is', title) 
+
+# values = st.slider(
+#   'Select a range of values', 
+#   0.0,100.0, (25.0,75.0)
+# )
+
+
+# col1, col2 , col3 = st.columns(3)
+
+# with col1:
+#     with st.form('Form1'):
+#         st.selectbox('Select flavor', ['Vanilla', 'Chocolate'], key=1)
+#         st.slider(label='Select intensity', min_value=0, max_value=100, key=4)
+#         submitted1 = st.form_submit_button('Submit 1')
+
+# with col2:
+#     with st.form('Form2'):
+#         st.selectbox('Select Topping', ['Almonds', 'Sprinkles'], key=2)
+#         st.slider(label='Select Intensity', min_value=0, max_value=100, key=3)
+#         submitted2 = st.form_submit_button('Submit 2')
+
+# with col3:
+#     with st.form('Form3'):
+#         st.selectbox('Select Position', ['Perrito', 'Sesentaynueve'], key=2)
+#         st.slider(label='Select Intensity', min_value=0, max_value=100, key=3)
+#         submitted2 = st.form_submit_button('Submit 2')
+
+
+# with st.expander("See Explanation"):
+#   st.write("esto se eirsdif ndsfnfngisdjfsrf]sdfsdsdgsfgdfghlrhthig frglhdfjg rg hjkgn srhgfhg ks")
+#   st.image("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/19870573-ead9-41ce-ba84-84acaf6569c8-1620828758.jpeg?crop=1xw:1xh;center,top&resize=480:*")
+
+# for i in range(len(persona)):  
+#   with st.expander(persona[i]):
+#     for j in range (len(posturas)):
+#       st.slider(label=posturas[j] , min_value=0, max_value=100, value=40 ,key=i*len(posturas)+j)
+
+
+if choice == 'Main':
+  show_main_page()
+elif choice == 'Postures':
+  show_postures()
