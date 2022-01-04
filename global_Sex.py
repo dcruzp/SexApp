fcd9cbd7393369973bfb532d6ab635bb6032160d
Javier Villar Alonso <80423975+Vyler-Lnidas@users.noreply.github.com>
2022-01-04 13:41:54 -0500

    ModelSex
    Base para la nueva visión del problema dado
    
    Contamos con Energia inicial que se basa en la resistencia de una persona, el placer que otorga cada postura a la persona y tambien el agotamiento que ocaciona cada postura a la persona determinada. Tambien llevamos una variable tiempo para determinar cuanto se dedico a cada postura.
    
    Llevamos  también el placer necesario para llegar al orgasmo, ya que sino no sabremos cuando cumplimos la meta, y por ultimo los datos de la postura y las personas participantes
    
    el objetivo de esto es que con los parámetros básicos conocidos para realizar el acto sexual, poder realizar dichos modelos (sin una excesiva carga de variables)

---
+++ global_Sex.py
@@ -0,0 +1,12 @@
+# Este trabajo se basa en hallar la mejor manera de que un grupo de persona 
+# realice un acto sexual (ya sea de dos personas o una orgia)
+
+Per = []  # : Personas que participan en el acto sexual
+Post = [] # : Posturas disponibles para el acto sexual
+
+Pij = [] # Placer que otorga => i : postura, j : persona
+Aij = [] # Agotamiento que otorga => i : postura, j : persona
+
+PO_i = [] # Placer necesario para que la persona llegue al orgasmo => i : persona
+Ei = [] # Energia inicial de la persona => i : persona
+Ti = [] # Tiempo de realizacion de cada postura => i : persona
---
+++ sexmodel.py
@@ -0,0 +1,19 @@
+import global_Sex as gs
+
+def Model_1():
+    return
+
+def Model_2():
+    return
+
+def Model_3():
+    return
+
+def Model_4():
+    return
+
+def Model_5():
+    return
+
+def Model_6():
+    return
