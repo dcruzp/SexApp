30abfacfdc420a4d061b93c2cc510458e4fabb3d
Daniel de la Cruz <danieldelacruzprieto@gmail.com>
2022-01-21 23:19:40 -0500

    midyfying the viewmodel2 for better aparence

--- viewmodel1.py
+++ viewmodel1.py
@@ -29,7 +29,7 @@ def show_modelo_1():
 
 
   menu_data = [
-    {'id': 'selectpositions','icon': "far fa-copy", 'label':"Posiciones" },
+    {'id': 'selectpositions','icon': "plus-square", 'label':"Posiciones" },
     {'id': 'selectpersons','icon':"plus-square",'label':"Personas"},
     {'id': 'ecut','icon':"plus-square",'label':"ECUT"},
     {'id': 'pgut','icon':"plus-square",'label':"PGUT"},
@@ -180,6 +180,8 @@ def show_modelo_1():
         NPPOO = st.session_state['NPPOO']
         participants = st.session_state['persons']
         optionsPositions = st.session_state['positions']
+
+        # st.write( st.session_state['ECUT'])
 
 
         result = solvepulp.Solve1stProblem(ECUT,PGUT,EIP,PIP,NPPOO,participants,optionsPositions)
